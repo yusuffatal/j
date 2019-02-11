80f055bf81e07e8b70d98be837eac804a8f8291f
yusuffatal <31797247+yusuffatal@users.noreply.github.com>
2019-02-12 06:11:27 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$config = [
+"imei" => "861930042100954",
+"sign" => 
+"426d4ef6c2022695c1fd8b2d89776b7f7e5ed920",
+"token" => 
+"38bc2efd6259b2e7c0c2980c1f1e2e79",
+"uuid" => 
+"b0b803c2a969713b"
+];
